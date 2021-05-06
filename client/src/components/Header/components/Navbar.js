646a0209cd0d631d96f2da4883646ca2e0553c0e
VietNguyen27 <nquocviet2307@gmail.com>
2021-05-06 22:38:17 +0700

    task 7

--- client/src/components/Header/components/Navbar.js
+++ client/src/components/Header/components/Navbar.js
@@ -3,8 +3,11 @@ import Windows from '../../../assets/windows-cover.jpg';
 import Mac from '../../../assets/mac-cover.jpg';
 import Linux from '../../../assets/linux-cover.jpg';
 import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const Navbar = ({ navbarItems }) => {
+  const { t } = useTranslation();
+
   return (
     <ul className='main-menu d-none d-md-flex'>
       {navbarItems.map((item, index) => {
@@ -18,25 +21,25 @@ const Navbar = ({ navbarItems }) => {
                 className='menu-link'
                 activeClassName='active'
               >
-                {label}
+                {t(label)}
                 <i className='ei ei-arrow_carrot-down'></i>
               </NavLink>
               <div className='submenu-content d-none d-md-block'>
                 <div className='submenu-inner'>
-                  <h1>All Categories</h1>
+                  <h1>{t('allCategories')}</h1>
                   <div className='row pb-3'>
                     {submenuItems.map((item, index) => {
                       const { label, path } = item;
                       return (
                         <div className='col-4' key={index}>
                           <NavLink exact to={path} className='submenu-item'>
-                            {label}
+                            {t(`categories.${label}`)}
                           </NavLink>
                         </div>
                       );
                     })}
                   </div>
-                  <h1>Browser All Games</h1>
+                  <h1>{t('browserAllGames')}</h1>
                   <div className='menu-collection'>
                     <div className='row'>
                       <div className='col-4'>
@@ -45,7 +48,7 @@ const Navbar = ({ navbarItems }) => {
                           className='menu-collection-item d-block'
                         >
                           <img src={Windows} alt='Games for windows' />
-                          <p>All games for Windows</p>
+                          <p>{t('allGamesForWindows')}</p>
                         </NavLink>
                       </div>
                       <div className='col-4'>
@@ -54,7 +57,7 @@ const Navbar = ({ navbarItems }) => {
                           className='menu-collection-item d-block'
                         >
                           <img src={Mac} alt='Games for mac' />
-                          <p>All games for Mac</p>
+                          <p>{t('allGamesForMac')}</p>
                         </NavLink>
                       </div>
                       <div className='col-4'>
@@ -63,7 +66,7 @@ const Navbar = ({ navbarItems }) => {
                           className='menu-collection-item d-block'
                         >
                           <img src={Linux} alt='Games for linux' />
-                          <p>All games for Linux</p>
+                          <p>{t('allGamesForLinux')}</p>
                         </NavLink>
                       </div>
                     </div>
@@ -81,7 +84,7 @@ const Navbar = ({ navbarItems }) => {
               className='menu-link'
               activeClassName='active'
             >
-              {label}
+              {t(label)}
             </NavLink>
           </li>
         );
---
+++ client/src/screens/HomeScreen/components/Hero.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+const Hero = () => {
+  const { t } = useTranslation();
+
+  return (
+    <section className='hero'>
+      <div className='hero-img'>
+        <div className='hero-text'>
+          <p className='pb-2'>{t('heroText')}</p>
+          <Link className='btn btn-primary' to='/products'>
+            {t('browserAllGames')}
+          </Link>
+        </div>
+      </div>
+      <div className='hero-genres'>
+        <div className='container'>
+          <div className='title'>Browser Genres</div>
+          <div className='genre-list'>
+            {genres.map((item, index) => {
+              const { label, img } = item;
+              return (
+                <div className='genre-item' key={index}>
+                  <div className='genre-content'>
+                    <img
+                      className='genre-img'
+                      src={img}
+                      alt={`${label} image`}
+                    />
+                    <div className='genre-text'>{t(`categories.${label}`)}</div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const genres = [
+  {
+    label: 'action',
+    img: 'https://s.gjcdn.net/assets/action.7447804b.png',
+  },
+  {
+    label: 'adventure',
+    img: 'https://s.gjcdn.net/assets/adventure.8420ba5c.png',
+  },
+  {
+    label: 'indie',
+    img: 'https://s.gjcdn.net/assets/fangame.c423343d.png',
+  },
+  {
+    label: 'rpg',
+    img: 'https://s.gjcdn.net/assets/rpg.52872556.png',
+  },
+  {
+    label: 'shooters',
+    img: 'https://s.gjcdn.net/assets/shooter.583bbf69.png',
+  },
+  {
+    label: 'simulation',
+    img: 'https://s.gjcdn.net/assets/arcade.3bff24da.png',
+  },
+  {
+    label: 'sportsRacing',
+    img: 'https://s.gjcdn.net/assets/sports.b83213db.png',
+  },
+  {
+    label: 'strategy',
+    img: 'https://s.gjcdn.net/assets/strategysim.7005c15c.png',
+  },
+];
+
+export default Hero;
